1f5c8835bdcdde08996bd781de984c63fd245baf
whyKirbyy <timschmid02@icloud.com>
2024-09-07 20:46:12 +0200

    some changes to svg element thingy

--- src/components/page/pageContactForm.tsx
+++ src/components/page/pageContactForm.tsx
@@ -6,29 +6,29 @@ import styles from "../../styles/components/pageContactForm.module.css";
 
 const PageContactForm: React.FC = () => {
     return (
-        <div className={styles.loginBox}>
-            <div className={styles.loginTitle}>
+        <div className={styles.contactBox}>
+            <div className={styles.contactTitle}>
                 CONTACT FORM
             </div>
-            <div className={styles.loginBoxItem}>
-                <div className={styles.loginBoxItemName}>
+            <div className={styles.contactBoxItem}>
+                <div className={styles.contactBoxItemName}>
                     EMAIL:
                 </div>
                 <PageTextField placeholder={"Enter your email"}/>
             </div>
-            <div className={styles.loginBoxItem}>
-                <div className={styles.loginBoxItemName}>
+            <div className={styles.contactBoxItem}>
+                <div className={styles.contactBoxItemName}>
                     NAME:
                 </div>
                 <PageTextField placeholder={"Enter your name"}/>
             </div>
-            <div className={styles.loginBoxItem2}>
-                <div className={styles.loginBoxItemName}>
+            <div className={styles.contactBoxItem2}>
+                <div className={styles.contactBoxItemName}>
                     Message:
                 </div>
                 <PageMessageField placeholder={"Enter your message"}/>
             </div>
-            <div className={styles.loginBoxItemButton}>
+            <div className={styles.contactBoxItemButton}>
                 <PageButton label={"SEND"} href={"/send"}/>
             </div>
         </div>
