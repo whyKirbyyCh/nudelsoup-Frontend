01f109ff8ddcc3ba1a933ada2495936a51542bac
whyKirbyy <timschmid02@icloud.com>
2024-09-07 20:55:20 +0200

    some changes to svg element thingy 2

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,9 +4,16 @@ import Header from "@/components/header/header";
 import PageRegisterBox from "@/components/page/pageRegisterBox";
 
 export default function Page() {
+    const navOptions = [
+        { id: 1, label: 'REVIEWS', href: '/reviews' },
+        { id: 2, label: 'FAQ', href: '/faq' },
+        { id: 3, label: 'PRICING', href: '/pricing' },
+        { id: 4, label: 'CONTACT', href: '/contact' },
+    ];
+
     return (
         <div className={styles.register}>
-            <Header iconSize={"small"} navOptions={[]} fontSizeVariant={"small"} showButtons={false}/>
+            <Header iconSize={"large"} navOptions={navOptions} fontSizeVariant={"large"} showButtons={true}/>
             <div className={styles.registerContentWrapper}>
                 <PageRegisterBox/>
             </div>
