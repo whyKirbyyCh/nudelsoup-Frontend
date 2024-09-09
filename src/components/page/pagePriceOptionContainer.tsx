2c3c0f2bf8aa4daf5dd07c659d7c3c31f47f359d
whyKirbyy <timschmid02@icloud.com>
2024-09-09 20:29:33 +0200

    updated the price page

--- src/components/page/pagePriceOptionContainer.tsx
+++ src/components/page/pagePriceOptionContainer.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import styles from "../../styles/components/pagePriceOptionContainer.module.css";
 import PageButton from "@/components/page/pageButton";
+import PageSVGElement from "@/components/page/pageSVGElement";
 
 interface PagePriceOptionBoxProps {
     title: string;
     text: string;
-    onClick?: () => void;
-    buttonText: string;
+    onClick1?: () => void;
+    onClick2?: () => void;
+    buttonText1: string;
+    buttonText2: string;
+    priceOption: number
 }
 
 
-const PagePriceOptionBox: React.FC<PagePriceOptionBoxProps> = ({ title, text, buttonText, onClick }) => {
+const PagePriceOptionBox: React.FC<PagePriceOptionBoxProps> = ({ title, text, buttonText1,buttonText2 , onClick1, onClick2, priceOption }) => {
     return (
         <div className={styles.priceOptionBox}>
             <div className={styles.priceOptionBoxTitle}>
@@ -20,8 +24,14 @@ const PagePriceOptionBox: React.FC<PagePriceOptionBoxProps> = ({ title, text, bu
                 <div className={styles.priceOptionBoxText}>
                     {text}
                 </div>
+                <div className={styles.priceSVG}>
+                    {priceOption === 1 && <PageSVGElement svgSrc={"price-option-one.svg"}/>}
+                    {priceOption === 2 && <PageSVGElement svgSrc={"price-option-two.svg"}/>}
+                    {priceOption === 3 && <PageSVGElement svgSrc={"price-option-three.svg"}/>}
+                </div>
                 <div className={styles.priceOptionBoxButton}>
-                    <PageButton label={buttonText} onClick={onClick}/>
+                    <PageButton label={buttonText2} onClick={onClick2}/>
+                    <PageButton label={buttonText1} onClick={onClick1}/>
                 </div>
             </div>
         </div>
--- src/components/page/pagePricingBox.tsx
+++ src/components/page/pagePricingBox.tsx
@@ -1,55 +1,93 @@
-import React from "react";
+"use client";
+
+import React, {useState} from "react";
 import styles from "../../styles/components/pagePricingBox.module.css";
 import PagePriceOptionBox from "../page/pagePriceOptionContainer";
 import PageCheckoutButton from "@/components/page/pageCheckoutButton";
+import PagePriceInformationBox from "@/components/page/pagePriceInformationBox";
 
 interface PagePricingBoxProps {
     title1: string;
     text1: string;
-    onClick1?: () => void;
+    onClick1: () => void;
     buttonText1: string;
 
     title2: string;
     text2: string;
-    onClick2?: () => void;
+    onClick2: () => void;
     buttonText2: string;
 
     title3: string;
     text3: string;
-    onClick3?: () => void;
+    onClick3: () => void;
     buttonText3: string;
 
     isButtonDisabled: boolean;
     onCheckoutError: () => void;
 }
 
-const PagePricingBox: React.FC<PagePricingBoxProps> = ({
-                                                           title1,
-                                                           text1,
-                                                           onClick1,
-                                                           buttonText1,
-                                                           title2,
-                                                           text2,
-                                                           onClick2,
-                                                           buttonText2,
-                                                           title3,
-                                                           text3,
-                                                           onClick3,
-                                                           buttonText3,
-                                                           isButtonDisabled,
-                                                           onCheckoutError,
-                                                       }) => {
+const PagePricingBox: React.FC<PagePricingBoxProps> = ({title1, text1, onClick1, buttonText1, title2, text2, onClick2, buttonText2, title3, text3, onClick3, buttonText3, isButtonDisabled, onCheckoutError}) => {
+    const[moreInfo, setMoreInfo] = useState(-1);
+
+    const moreInfo1 = () => {
+        setMoreInfo(1);
+    }
+
+    const moreInfo2 = () => {
+        setMoreInfo(2);
+    }
+
+    const moreInfo3 = () => {
+        setMoreInfo(3);
+    }
+
+
+    const lessInfo = () => {
+        setMoreInfo(-1);
+    }
+
+    const lessInfo1 = () => {
+        setMoreInfo(-1);
+        onClick1();
+    }
+
+    const lessInfo2 = () => {
+        setMoreInfo(-1);
+        onClick2();
+    }
+
+    const lessInfo3 = () => {
+        setMoreInfo(-1);
+        onClick3();
+    }
+
     return (
         <div className={styles.pricingBox}>
             <div className={styles.pricingBoxContent}>
                 <div>
                     <div className={styles.pricingBoxSubtitle}>*if you are still hungry you can always order more</div>
                 </div>
+                {moreInfo === -1 &&
                 <div className={styles.pricingBoxContainer}>
-                    <PagePriceOptionBox title={title1} text={text1} buttonText={buttonText1} onClick={onClick1} />
-                    <PagePriceOptionBox title={title2} text={text2} buttonText={buttonText2} onClick={onClick2} />
-                    <PagePriceOptionBox title={title3} text={text3} buttonText={buttonText3} onClick={onClick3} />
-                </div>
+                    <PagePriceOptionBox title={title1} text={text1} buttonText1={buttonText1} onClick1={onClick1} buttonText2={"MORE INFO"} onClick2={moreInfo1} priceOption={1}/>
+                    <PagePriceOptionBox title={title2} text={text2} buttonText1={buttonText2} onClick1={onClick2} buttonText2={"MORE INFO"} onClick2={moreInfo2} priceOption={2}/>
+                    <PagePriceOptionBox title={title3} text={text3} buttonText1={buttonText3} onClick1={onClick3} buttonText2={"MORE INFO"} onClick2={moreInfo3} priceOption={3}/>
+                </div>}
+                {moreInfo === 1 &&
+                    <div className={styles.pricingBoxContainer}>
+                        <PagePriceOptionBox title={title1} text={text1} buttonText1={buttonText1} onClick1={lessInfo1} buttonText2={"LESS INFO"} onClick2={lessInfo} priceOption={1}/>
+                        <PagePriceInformationBox title={"OUR 'DINNER FOR ONE' OPTION EXPLAINED"} text={"this is text"}/>
+                    </div>}
+                {moreInfo === 2 &&
+                    <div className={styles.pricingBoxContainer}>
+                        <PagePriceOptionBox title={title2} text={text2} buttonText1={buttonText2} onClick1={lessInfo2} buttonText2={"LESS INFO"} onClick2={lessInfo} priceOption={2}/>
+                        <PagePriceInformationBox title={"OUR 'FAMILY SIZED MEAL' OPTION EXPLAINED"} text={"this is text"}/>
+                    </div>}
+                {moreInfo === 3 &&
+                    <div className={styles.pricingBoxContainer}>
+                        <PagePriceOptionBox title={title3} text={text3} buttonText1={buttonText3} onClick1={lessInfo3} buttonText2={"MORE INFO"} onClick2={lessInfo} priceOption={3}/>
+                        <PagePriceInformationBox title={"OUR 'DELUXE PARTY BUFFET' OPTION EXPLAINED"} text={"this is text"}/>
+                    </div>}
                 <div className={styles.pricingBoxButton}>
                     <PageCheckoutButton label={"TO CHECKOUT"} onClick={isButtonDisabled ? onCheckoutError : undefined} />
                 </div>
