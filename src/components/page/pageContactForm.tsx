cf21df1354badfda29fe091a62dcc9e1de5c5b3b
whyKirbyy <timschmid02@icloud.com>
2024-09-09 18:42:56 +0200

    contact page now contains some checks

--- src/components/page/pageContactForm.tsx
+++ src/components/page/pageContactForm.tsx
@@ -1,10 +1,40 @@
-import React from "react";
+"use client"
+
+import React, {useState} from "react";
 import PageTextField from "@/components/page/pageTextField";
 import PageMessageField from "@/components/page/pageMessageField";
-import PageButton from "@/components/page/pageButton";
+import PageCheckoutButton from "@/components/page/pageCheckoutButton";
 import styles from "../../styles/components/pageContactForm.module.css";
 
+//TODO: implement the email sending logic
 const PageContactForm: React.FC = () => {
+    const [message, setMessage] = useState("")
+    const [email, setEmail] = useState("")
+    const [name, setName] = useState("")
+    const [contactErrorMessage, setContactErrorMessage] = useState("")
+
+    const onClick = () => {
+        setContactErrorMessage("");
+
+        if (!email || !name || !message) {
+            setContactErrorMessage("Please fill out all fields.");
+            return;
+        }
+        else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+            setContactErrorMessage("Please enter a valid email address.");
+            return;
+        } else if (!/^[a-zA-Z\s]+$/.test(name)) {
+            setContactErrorMessage("Name can only contain letters and spaces.");
+            return;
+        } else if (message.length < 10) {
+            setContactErrorMessage("Your message seems to be too short.");
+            return;
+        }
+
+        //send mail
+        return
+    };
+
     return (
         <div className={styles.contactBox}>
             <div className={styles.contactTitle}>
@@ -14,22 +44,23 @@ const PageContactForm: React.FC = () => {
                 <div className={styles.contactBoxItemName}>
                     EMAIL:
                 </div>
-                <PageTextField placeholder={"Enter your email"}/>
+                <PageTextField placeholder={"Enter your email"} value={email} onChange={(event) => setEmail(event.target.value)}/>
             </div>
             <div className={styles.contactBoxItem}>
                 <div className={styles.contactBoxItemName}>
                     NAME:
                 </div>
-                <PageTextField placeholder={"Enter your name"}/>
+                <PageTextField placeholder={"Enter your name"} value={name} onChange={(event) => setName(event.target.value)}/>
             </div>
             <div className={styles.contactBoxItem2}>
                 <div className={styles.contactBoxItemName}>
                     Message:
                 </div>
-                <PageMessageField placeholder={"Enter your message"}/>
+                <PageMessageField placeholder={"Enter your message"} value={message} onChange={(event) => setMessage(event.target.value)}/>
             </div>
+            {contactErrorMessage && <div className={styles.errorMessage}>{contactErrorMessage}</div>}
             <div className={styles.contactBoxItemButton}>
-                <PageButton label={"SEND"} href={"/send"}/>
+                <PageCheckoutButton label={"SEND"} onClick={onClick}/>
             </div>
         </div>
     );
