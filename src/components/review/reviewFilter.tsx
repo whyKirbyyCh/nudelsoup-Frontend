fb973172f45d0298aa6340fa4f4094915e029765
whyKirbyy <timschmid02@icloud.com>
2024-09-12 15:47:52 +0200

    some more stuff in the review site

---
+++ src/components/review/reviewFilter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "../../styles/components/review/reviewFilter.module.css";
+
+const ReviewFilter = () => {
+    return (
+        <div className={styles.reviewFilter}>
+            Filter
+        </div>
+    )
+}
+
+export default ReviewFilter
--- src/components/review/reviewPageContainer.tsx
+++ src/components/review/reviewPageContainer.tsx
@@ -2,12 +2,18 @@ import React from "react";
 import styles from "../../styles/components/review/reviewPageContainer.module.css";
 import ReviewStatisticsContainer from "./reviewStatisticsContainer"
 import ReviewWritingContainer from "./reviewWritingContainer";
+import ReviewSelectionContainer from "@/components/review/reviewSelectionContainer";
 
 const ReviewPageContainer: React.FC = () => {
     return (
         <div className={styles.reviewPageContainer}>
-            <ReviewStatisticsContainer/>
-            <ReviewWritingContainer/>
+            <div className={styles.reviewPageContainerTop}>
+                <ReviewStatisticsContainer/>
+                <ReviewWritingContainer/>
+            </div>
+            <div>
+                <ReviewSelectionContainer/>
+            </div>
         </div>
     );
 }
