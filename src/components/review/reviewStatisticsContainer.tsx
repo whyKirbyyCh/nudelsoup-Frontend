1f19a5bfa273ca3640aed0016e6947d834f9793d
whyKirbyy <timschmid02@icloud.com>
2024-09-12 16:24:08 +0200

    updated the statistics container

--- src/components/review/reviewStatisticsContainer.tsx
+++ src/components/review/reviewStatisticsContainer.tsx
@@ -5,11 +5,17 @@ import ReviewStars from "@/components/review/reviewStars";
 const ReviewStatisticsContainer: React.FC = () => {
     return (
         <div className={styles.reviewStatisticsContainer}>
-            <div>
+            <div className={styles.reviewStatisticsContainerTitle}>
                 REVIEW STATISTICS
             </div>
             <div className={styles.reviewStatisticsContainerTop}>
                 <div className={styles.reviewStatisticsContainerTopLeft}>
+                    <img
+                        src={"/average.svg"}
+                        alt={"average symbol"}
+                        className={styles.averageSymbol}
+                    />
+                    <div className={styles.averageSymbolPoints}>: </div>
                     <ReviewStars rating={4.592}/>
                 </div>
                 <div className={styles.reviewStatisticsContainerTopRight}>
@@ -18,6 +24,15 @@ const ReviewStatisticsContainer: React.FC = () => {
             </div>
             <div className={styles.reviewStatisticsContainerBottom}>
                 <div className={styles.reviewStatisticsContainerBottomLeft}>
+                    <img
+                        src={"/amount.svg"}
+                        alt={"amount symbol"}
+                        className={styles.amountSymbol}
+                    />
+                    <div className={styles.amountSymbolPoints}>:</div>
+                    <div className={styles.reviewAmounts}>
+                        1175
+                    </div>
                 </div>
                 <div className={styles.reviewStatisticsContainerBottomRight}>
 
--- src/components/review/reviewWritingContainer.tsx
+++ src/components/review/reviewWritingContainer.tsx
@@ -4,22 +4,10 @@ import styles from "../../styles/components/review/reviewWritingContainer.module
 const ReviewWritingContainer: React.FC = () => {
     return (
         <div className={styles.reviewWritingContainer}>
-            <div>
+            <div className={styles.reviewWritingContainerTitle}>
                 WRITE YOUR REVIEW
             </div>
-            <div className={styles.reviewWritingContainerTop}>
-                <div className={styles.reviewWritingContainerTopLeft}>
-                    hi
-                </div>
-                <div className={styles.reviewWritingContainerTopRight}>
-                </div>
-            </div>
-            <div className={styles.reviewWritingContainerBottom}>
-                <div className={styles.reviewWritingContainerBottomLeft}>
-                </div>
-                <div className={styles.reviewWritingContainerBottomRight}>
-
-                </div>
+            <div className={styles.reviewWritingContainerInput}>
             </div>
         </div>
     );
