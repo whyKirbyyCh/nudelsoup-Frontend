18623c3c5e1d7912a0ede67a1f1eb2f887d25194
whyKirbyy <timschmid02@icloud.com>
2024-09-18 13:12:35 +0200

    stuff

--- src/pages/api/userRegister.ts
+++ src/pages/api/userRegister.ts
@@ -80,6 +80,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
         return res.status(201).json({ message: 'User registered successfully', userId });
     } catch (error) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({ message: 'Internal server error' + error });
     }
 }
