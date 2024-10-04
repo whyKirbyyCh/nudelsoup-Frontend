625e885ca6eaccd833d92c22e225658f2da954f7
whyKirbyy <timschmid02@icloud.com>
2024-10-04 23:34:19 +0200

    closes #37

--- src/app/organisation-overview/page.tsx
+++ src/app/organisation-overview/page.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./organisation-overviewPage.module.css";
+import Header from "@/components/header/header";
 
 export default function Page() {
+    const payingCustomerNavOptions = [
+        { id: 1, label: "ORGANISATION", href: "/organisation-overview" },
+        { id: 2, label: "PRODUCTS", href: "/product-overview" },
+        { id: 3, label: "CAMPAIGNS", href: "/campaign-overview" },
+        { id: 4, label: "ANALYTICS", href: "/analytics" },
+    ];
+
     return (
         <div className={styles.container}>
-            <h1>Organisation Overview</h1>
+            <div>
+                <Header iconSize="large" navOptions={payingCustomerNavOptions} fontSizeVariant="large" showButtons={true}/>
+            </div>
         </div>
     );
 }
