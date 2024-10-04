9245343068c0885a00b6cbb305d47864d20e25a8
whyKirbyy <timschmid02@icloud.com>
2024-10-04 22:25:44 +0200

    next page tab is done

--- src/components/account/accountCreationNextStepsContainer.tsx
+++ src/components/account/accountCreationNextStepsContainer.tsx
@@ -3,8 +3,27 @@ import styles from "../../styles/components/account/accountCreationNextSteps.mod
 
 const AccountCreationNextStepsContainer: React.FC = ({}) => {
     return(
-        <div>Hi</div>
+        <div className={styles.nextStepsContainer}>
+            <div className={styles.nextStepsContainerHeader}>
+                <div className={styles.nextStepsContainerTitle}>NEXT STEPS</div>
+                <div className={styles.nextStepsContainerDescription}>
+                    Now that you have completed the account setup, you can start creating your first campaign. Please
+                    follow the instructions below to create a campaign.
+                    <br/><br/>
+                    1. Click the "Create Campaign" button on the top right of the page.
+                    <br/><br/>
+                    2. Fill out the campaign details, including the name, description, and desired audience.
+                    <br/><br/>
+                    3. Choose the type of campaign you want to create.
+                    <br/><br/>
+                    4. Upload your campaign content, such as images or videos.
+                    <br/><br/>
+                    5. Set your campaign budget and duration.
+                    <br/><br/>
+                    6. Click "Create Campaign" to submit your campaign.
+                </div>
+            </div>
+        </div>
     );
 };
-
 export default AccountCreationNextStepsContainer
