e1abdf5c519d1fb2c51bd85ab17f1773149cdf82
whyKirbyy <timschmid02@icloud.com>
2024-10-04 23:29:53 +0200

    started work on the organisation-overview page

---
+++ src/app/organisation-overview/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./organisation-overviewPage.module.css";
+
+export default function Page() {
+    return (
+        <div className={styles.container}>
+            <h1>Organisation Overview</h1>
+        </div>
+    );
+}
