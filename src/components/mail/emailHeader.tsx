db235a332e1af98cbcf502c4e47f4f0394061ebe
whyKirbyy <timschmid02@icloud.com>
2024-10-16 08:41:49 +0200

    mail template v1 done

---
+++ src/components/mail/emailHeader.tsx
@@ -0,0 +1,18 @@
+const EmailHeader = () => {
+    return `
+    <div style="
+      width: 100%;
+      background-color: #87a4aa;
+      padding: 10px 0;
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+      color: #ffffff;
+      height: 5em;
+    ">
+      nudelsoup
+    </div>
+  `;
+};
+
+export default EmailHeader;
